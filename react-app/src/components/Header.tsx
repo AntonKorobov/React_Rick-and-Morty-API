c296c60508280fd83c8a3b9ef2fe6aa968f0e6e4
NewAnton <anton1korobov1@gmail.com>
2022-10-07 18:42:31 +0300

    fix: connect tsx file to html

---
+++ react-app/src/components/Header.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export default function Header() {
+  return (
+    <header>
+      <nav>
+        <ul>
+          <li>Main page</li>
+          <li>About us</li>
+          <li>404</li>
+        </ul>
+      </nav>
+      <h1>Current page is:</h1>
+    </header>
+  );
+}
