2931a635abc1a04c9c7410c12226ae92cd205e0c
NewAnton <1anton1korobov@gmail.com>
2022-10-11 11:52:36 +0300

    refactor: add labels to inputs

--- react-app/src/components/Forms.tsx
+++ react-app/src/components/Forms.tsx
@@ -51,30 +51,102 @@ export default class Forms extends Component {
       <section className="forms">
         <h1>Forms</h1>
         <form className="card-creator-form">
-          <input
-            type="text"
-            name="title"
-            className="card-creator-form__title"
-            value={this.state.title}
-            placeholder="Title..."
-            onChange={this.handleChangeInput}
-          />
-          <input
-            type="text"
-            name="author"
-            className="card-creator-form__author"
-            value={this.state.author}
-            placeholder="Author..."
-            onChange={this.handleChangeInput}
-          />
-          <input
-            type="text"
-            name="publisher"
-            className="card-creator-form__publisher"
-            value={this.state.publisher}
-            placeholder="Publisher..."
-            onChange={this.handleChangeInput}
-          />
+          <label className="card-creator-form__title input-element">
+            Title:
+            <input
+              type="text"
+              name="title"
+              className="input-element__input"
+              value={this.state.title}
+              placeholder="Title..."
+              onChange={this.handleChangeInput}
+            />
+          </label>
+          <label className="card-creator-form__author input-element">
+            Author:
+            <input
+              type="text"
+              name="author"
+              className="input-element__input"
+              value={this.state.author}
+              placeholder="Author..."
+              onChange={this.handleChangeInput}
+            />
+          </label>
+          <label className="card-creator-form__publisher input-element">
+            Publisher:
+            <input
+              type="text"
+              name="publisher"
+              className="input-element__input"
+              value={this.state.publisher}
+              placeholder="Publisher..."
+              onChange={this.handleChangeInput}
+            />
+          </label>
+          <label className="card-creator-form__pages input-element">
+            Pages:
+            <input
+              type="number"
+              name="pages"
+              className="input-element__input"
+              value={this.state.pages}
+              onChange={this.handleChangeInput}
+              min="1"
+              max="10000"
+            />
+          </label>
+          <label className="card-creator-form__price input-element">
+            Price:
+            <input
+              type="number"
+              name="price"
+              className="input-element__input"
+              value={this.state.price}
+              onChange={this.handleChangeInput}
+              min="1"
+              max="1000"
+            />
+          </label>
+          <label className="card-creator-form__language input-element">
+            Language:
+            <select
+              name="language"
+              className="input-element__select"
+              value={this.state.language}
+              placeholder="Language..."
+              onChange={this.handleChangeInput}
+            >
+              <option value="English">English</option>
+              <option value="German">German</option>
+              <option value="Russian">Russian</option>
+              <option value="Spanish">Spanish</option>
+            </select>
+          </label>
+          <label className="card-creator-form__coverType input-element">
+            Cover type:
+            <select
+              name="coverType"
+              className="input-element__select"
+              value={this.state.coverType}
+              placeholder="CoverType..."
+              onChange={this.handleChangeInput}
+            >
+              <option value="Paperback">Paperback</option>
+              <option value="Hardcover">Hardcover</option>
+              <option value="Prebound">Prebound</option>
+            </select>
+          </label>
+          <label className="card-creator-form__written-switch switch">
+            <input
+              className="switch__input"
+              type="checkbox"
+              name="written"
+              defaultChecked={false} //uncontrolled
+              onChange={this.handleChangeInput}
+            />
+            <span className="switch__slider"></span>
+          </label>
           <div className="card-creator-form__category">
             <Checkbox
               className={'card-creator-form__category-checkbox'}
@@ -112,57 +184,6 @@ export default class Forms extends Component {
             placeholder="Description..."
             onChange={this.handleChangeInput}
           />
-          <input
-            type="number"
-            name="pages"
-            className="card-creator-form__pages"
-            value={this.state.pages}
-            onChange={this.handleChangeInput}
-            min="1"
-            max="10000"
-          />
-          <input
-            type="number"
-            name="price"
-            className="card-creator-form__price"
-            value={this.state.price}
-            onChange={this.handleChangeInput}
-            min="1"
-            max="1000"
-          />
-          <select
-            name="language"
-            className="card-creator-form__language"
-            value={this.state.language}
-            placeholder="Language..."
-            onChange={this.handleChangeInput}
-          >
-            <option value="English">English</option>
-            <option value="German">German</option>
-            <option value="Russian">Russian</option>
-            <option value="Spanish">Spanish</option>
-          </select>
-          <select
-            name="coverType"
-            className="card-creator-form__coverType"
-            value={this.state.coverType}
-            placeholder="CoverType..."
-            onChange={this.handleChangeInput}
-          >
-            <option value="Paperback">Paperback</option>
-            <option value="Hardcover">Hardcover</option>
-            <option value="Prebound">Prebound</option>
-          </select>
-          <label className="card-creator-form__written-switch switch">
-            <input
-              className="switch__input"
-              type="checkbox"
-              name="written"
-              defaultChecked={false} //uncontrolled
-              onChange={this.handleChangeInput}
-            />
-            <span className="switch__slider"></span>
-          </label>
           <FileUpload className={'card-creator-form__img'} />
         </form>
       </section>
