364d27058eaab16e5bd2b9dfb77a38f797322956
NewAnton <1anton1korobov@gmail.com>
2022-10-20 11:18:16 +0300

    refactor: optimize repeating code

--- react-app/src/pages/Main_Page/Main_Page.tsx
+++ react-app/src/pages/Main_Page/Main_Page.tsx
@@ -22,6 +22,27 @@ export default class MainPage extends Component {
     isLoadingError: false,
   };
 
+  apiGetCharacter = async (name: string, page = 1) => {
+    this.setState({ isLoaded: false, isLoadingError: false, characters: [] });
+    try {
+      const response = await fetch(
+        `https://rickandmortyapi.com/api/character/?page=${page}&name=${name}`
+      );
+      if (response.status === 200) {
+        const data: APICharacterInterface = await response.json();
+        this.setState({ characters: data.results, isLoadingError: false });
+        console.log(data);
+      } else {
+        this.setState({ isLoaded: true, isLoadingError: true });
+      }
+      setTimeout(() => {
+        this.setState({ isLoaded: true });
+      }, 1000);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   cardGenerator = (array: APISingleCharacterInterface[]): JSX.Element[] => {
     return array.map((elem, index) => <Card key={elem.id} info={array[index]} />);
   };
@@ -44,24 +65,7 @@ export default class MainPage extends Component {
   };
 
   onPageChange = async (pageNumber: number) => {
-    this.setState({ isLoaded: false, isLoadingError: false, characters: [] });
-    try {
-      const response = await fetch(
-        `https://rickandmortyapi.com/api/character/?page=${pageNumber}&name=${this.state.searchBarInput}`
-      );
-      if (response.status === 200) {
-        const data: APICharacterInterface = await response.json();
-        this.setState({ characters: data.results, isLoadingError: false });
-        console.log(data);
-      } else {
-        this.setState({ isLoaded: true, isLoadingError: true });
-      }
-      setTimeout(() => {
-        this.setState({ isLoaded: true });
-      }, 1000);
-    } catch (error) {
-      console.log(error);
-    }
+    this.apiGetCharacter(this.state.searchBarInput, pageNumber);
   };
 
   handleChangeSearchBar = (event: { target: { name: string; value: string } }) => {
@@ -72,49 +76,13 @@ export default class MainPage extends Component {
 
   searchBarOnSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-    localStorage.setItem('searchBarInput', this.state.searchBarInput); // !!!
-    this.setState({ isLoaded: false, isLoadingError: false, characters: [] });
-    try {
-      const response = await fetch(
-        `https://rickandmortyapi.com/api/character/?name=${this.state.searchBarInput}`
-      );
-      if (response.status === 200) {
-        const data: APICharacterInterface = await response.json();
-        this.setState({ characters: data.results, isLoadingError: false });
-        console.log(data);
-      } else {
-        this.setState({ isLoaded: true, isLoadingError: true });
-      }
-      setTimeout(() => {
-        this.setState({ isLoaded: true });
-      }, 1000);
-    } catch (error) {
-      console.log(error);
-    }
+    localStorage.setItem('searchBarInput', this.state.searchBarInput); //!!!
+    this.apiGetCharacter(this.state.searchBarInput);
   };
 
   async componentDidMount(): Promise<void> {
     this.setState({ searchBarInput: localStorage.getItem('searchBarInput') || '' });
-    this.setState({ isLoaded: false, isLoadingError: false, characters: [] });
-    try {
-      const response = await fetch(
-        `https://rickandmortyapi.com/api/character/?page=${this.state.currentPage}&name=${
-          localStorage.getItem('searchBarInput') || '' //state is empty!!!???
-        }`
-      );
-      if (response.status === 200) {
-        const data: APICharacterInterface = await response.json();
-        this.setState({ characters: data.results, isLoadingError: false });
-        console.log(data);
-      } else {
-        this.setState({ isLoaded: true, isLoadingError: true });
-      }
-      setTimeout(() => {
-        this.setState({ isLoaded: true });
-      }, 1000);
-    } catch (error) {
-      console.log(error);
-    }
+    this.apiGetCharacter(localStorage.getItem('searchBarInput') || '', this.state.currentPage);
   }
 
   componentWillUnmount(): void {
