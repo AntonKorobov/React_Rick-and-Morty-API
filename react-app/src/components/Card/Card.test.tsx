42974c47f36b87a145793aecb0455f87f1d0211d
NewAnton <anton1korobov1@gmail.com>
2022-10-24 20:17:15 +0300

    feat: add api mock to test

--- react-app/src/components/Card/Card.test.tsx
+++ react-app/src/components/Card/Card.test.tsx
@@ -1,61 +1,71 @@
-// import React from 'react';
-// import { render, screen } from '@testing-library/react';
-// import { Card } from './Card';
-// import { APICharacterInterface } from 'data/API_Interface';
-// import fetchMock from 'fetch-mock';
-// import { mockAPI } from "./mockAPI";
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import { Card } from './Card';
+import { APICharacterInterface } from 'data/API_Interface';
 
-// const response = await fetch(`https://rickandmortyapi.com/api/character/?page=${0}`);
-// const data: APICharacterInterface = await response.json();
-// const data: APICharacterInterface = fetchMock.mock(
-//   'https://rickandmortyapi.com/api/character/?page=${0}',
-//   {
-//     info: {
-//       count: 0,
-//       pages: 0,
-//       next: '',
-//       prev: null,
-//     },
-//     results: [
-//       {
-//         id: 0,
-//         name: 'Test-name',
-//         status: 'Test-status',
-//         species: 'Test-species',
-//         type: 'Test-type',
-//         gender: 'Test-gender',
-//         origin: {
-//           name: '',
-//           url: '',
-//         },
-//         location: {
-//           name: '',
-//           url: '',
-//         },
-//         image: '',
-//         episode: ['', ''],
-//         url: '',
-//         created: '',
-//       },
-//     ],
-//   }
-// );
+const testCharacter: APICharacterInterface = {
+  info: {
+    count: 0,
+    pages: 0,
+    next: '',
+    prev: null,
+  },
+  results: [
+    {
+      id: 0,
+      name: 'Test-name',
+      status: 'Test-status',
+      species: 'Test-species',
+      type: 'Test-type',
+      gender: 'Test-gender',
+      origin: {
+        name: '',
+        url: '',
+      },
+      location: {
+        name: '',
+        url: '',
+      },
+      image: '',
+      episode: ['', ''],
+      url: '',
+      created: '',
+    },
+  ],
+};
 
-// describe('render one card', () => {
-//   test('renders cards', () => {
-//     render(<Card info={data.results[0]} />);
-//     const cardId = screen.getByTestId('card');
-//     expect(cardId).toBeInTheDocument();
-//   });
-//   test('cards should have a name', () => {
-//     render(<Card info={data.results[0]} />);
-//     const cardTitle = screen.getByText(data.results[0].name);
-//     expect(cardTitle).toBeDefined();
-//   });
-//   test('cards should have src and alt attributes', () => {
-//     render(<Card info={data.results[0]} />);
-//     const cardImg = screen.getByRole('img');
-//     expect(cardImg).toHaveAttribute('src');
-//     expect(cardImg).toHaveAttribute('alt');
-//   });
-// });
+global.fetch = jest.fn(() =>
+  Promise.resolve({
+    json: () => Promise.resolve(testCharacter),
+  })
+) as jest.Mock;
+
+describe('renders one card', () => {
+  test('renders cards', async () => {
+    //`https://rickandmortyapi.com/api/character/?page=${page}&name=${name}`
+
+    const response = await fetch(`https://rickandmortyapi.com/api/character/?page=0`);
+    const data: APICharacterInterface = await response.json();
+
+    render(<Card info={data.results[0]} />);
+    const cardId = screen.getByTestId('card');
+    expect(cardId).toBeInTheDocument();
+  });
+  test('cards should have a name', async () => {
+    const response = await fetch(`https://rickandmortyapi.com/api/character/?page=0`);
+    const data: APICharacterInterface = await response.json();
+
+    render(<Card info={data.results[0]} />);
+    const cardTitle = screen.getByText(data.results[0].name);
+    expect(cardTitle).toBeDefined();
+  });
+  test('cards should have src and alt attributes', async () => {
+    const response = await fetch(`https://rickandmortyapi.com/api/character/?page=0`);
+    const data: APICharacterInterface = await response.json();
+
+    render(<Card info={data.results[0]} />);
+    const cardImg = screen.getByRole('img');
+    expect(cardImg).toHaveAttribute('src');
+    expect(cardImg).toHaveAttribute('alt');
+  });
+});
