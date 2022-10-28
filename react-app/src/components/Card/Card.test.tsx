3e844b29017a94595621d43ebe95ac1760e0cf17
NewAnton <anton1korobov1@gmail.com>
2022-10-28 09:24:46 +0300

    refactor: use API module to fetch data

--- react-app/src/components/Card/Card.test.tsx
+++ react-app/src/components/Card/Card.test.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import { Card } from './Card';
 import { APICharacterInterface } from 'data/API_Interface';
+import { API } from 'api/API';
 
 const testCharacter: APICharacterInterface = {
   info: {
@@ -36,34 +37,34 @@ const testCharacter: APICharacterInterface = {
 
 function myFetch() {
   return Promise.resolve({
+    status: 200,
     json: () => Promise.resolve(testCharacter),
   });
 }
 
 global.fetch = myFetch as jest.Mock;
 
+async function renderCard() {
+  const data = await API.getCharacter('', 0);
+  if (data) {
+    return <Card info={data.results[0]} />;
+  }
+  return <></>;
+}
+
 describe('renders one card', () => {
   test('renders cards', async () => {
-    const response = await fetch(`https://rickandmortyapi.com/api/character/?page=0`);
-    const data: APICharacterInterface = await response.json();
-
-    render(<Card info={data.results[0]} />);
+    render(await renderCard());
     const cardId = screen.getByTestId('card');
     expect(cardId).toBeInTheDocument();
   });
   test('cards should have a name', async () => {
-    const response = await fetch(`https://rickandmortyapi.com/api/character/?page=0`);
-    const data: APICharacterInterface = await response.json();
-
-    render(<Card info={data.results[0]} />);
-    const cardTitle = screen.getByText(data.results[0].name);
+    render(await renderCard());
+    const cardTitle = screen.getByText(testCharacter.results[0].name);
     expect(cardTitle).toBeDefined();
   });
   test('cards should have src and alt attributes', async () => {
-    const response = await fetch(`https://rickandmortyapi.com/api/character/?page=0`);
-    const data: APICharacterInterface = await response.json();
-
-    render(<Card info={data.results[0]} />);
+    render(await renderCard());
     const cardImg = screen.getByRole('img');
     expect(cardImg).toHaveAttribute('src');
     expect(cardImg).toHaveAttribute('alt');
